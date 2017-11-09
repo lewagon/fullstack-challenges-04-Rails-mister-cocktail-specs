ef2535f45c2ea6f8d4f6d332105272062fb59374
Alex Benoit <alexander.benoit@outlook.com>
2017-11-09 12:20:35 +0000

    removed

--- models/ingredient_spec.rb
+++ models/ingredient_spec.rb
@@ -30,12 +30,4 @@ RSpec.describe "Ingredient", :type => :model do
     expect(ingredient).to respond_to(:doses)
     expect(ingredient.doses.count).to eq(0)
   end
-
-  it "should not be able to destroy self if has dose children" do
-    cocktail = Cocktail.create!(valid_attributes)
-    ingredient = Ingredient.create!(name: "ice")
-    cocktail.doses.create(ingredient: ingredient, description: "A lot!")
-
-    expect { ingredient.destroy }.to raise_error(ActiveRecord::InvalidForeignKey)
-  end
 end
